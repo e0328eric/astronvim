c39383ee927742326ad6ab970af8d179c8326a3d
Sungbae Jeong <almagest0328@gmail.com>
2023-04-20 18:42:17 +0900

    fix indent size

---
+++ plugins/indent-o-matic.lua
@@ -0,0 +1,17 @@
+return {
+  "Darazaki/indent-o-matic",
+  opts = {
+    max_lines = 2048,
+    standard_widths = { 2, 4, 8 },
+    skip_multiline = true,
+    filetype_rust = {
+      standard_widths = { 4 },
+    },
+    filetype_c = {
+      standard_widths = { 4 },
+    },
+    filetype_cpp = {
+      standard_widths = { 4 },
+    },
+  },
+}
